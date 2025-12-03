befb308dae7f0bac14ecf514b6ad805f99994e8d
Salim Talal Salim alRawas <92043910+Srawas1996@users.noreply.github.com>
2025-12-03 16:13:51 +0300

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "Steps"
+@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"},
         ,plugin = {"pretty","html:target/reports/report.html"}
 //        ,dryRun = true
         ,monochrome = true
