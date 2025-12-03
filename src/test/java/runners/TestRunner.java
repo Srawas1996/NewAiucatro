07fc4f5238f5af651911de395d29e6f2ee666bae
SalimAlRawas <salim96tr@gmail.com>
2025-12-03 14:53:56 +0300

    Fixing some bugs

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "Steps"
         ,tags = "@Regression"
-//        ,plugin = {"pretty","html:target/reports/report.html", "json:target/report/jsons/report.json"}
+        ,plugin = {"pretty","html:target/reports/report.html", "json:target/report/jsons/report.json"}
 //        ,dryRun = true
        )
 public class TestRunner {
